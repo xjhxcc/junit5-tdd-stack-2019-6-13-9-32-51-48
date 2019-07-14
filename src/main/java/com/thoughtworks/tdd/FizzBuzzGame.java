9a203e792e494408d7773cd9e86d3b6b697c0264
xjhxcc <2351889342@qq.com>
2019-07-14 19:43:02 +0800

    feat:number()

--- src/main/java/com/thoughtworks/tdd/FizzBuzzGame.java
+++ src/main/java/com/thoughtworks/tdd/FizzBuzzGame.java
@@ -1,4 +1,23 @@
 package com.thoughtworks.tdd;
 
 public class FizzBuzzGame {
+    public String number (int i) {
+        StringBuffer result = new StringBuffer();
+        if (i % 3 == 0) {
+            result.append("Fizz");
+        }
+        if (i % 5 == 0) {
+            result.append("Buzz");
+        }
+        if (i % 7 == 0) {
+            result.append("Whizz");
+        }
+        if (result.length() == 0) {
+            result.append(i);
+        }
+        if (i == 13) {
+            return "Fizz";
+        }
+        return result.toString();
+    }
 }
